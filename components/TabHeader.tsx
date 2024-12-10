cf5f3714c54ca8fbe46830d8051950b21d6191f4
qhuongng <nq.huong.2304@gmail.com>
2024-12-10 16:53:07 +0700

    Add TabHeader component

---
+++ components/TabHeader.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Tabs, Text } from "@mantine/core";
+import React from "react";
+
+interface TabHeaderProps {
+    tabs: { label: string; content: JSX.Element }[];
+    mt: string | number;
+}
+
+const TabHeader: React.FC<TabHeaderProps> = ({ tabs, mt }) => {
+    return (
+        <Tabs mt={mt} variant="default" radius="md" defaultValue={tabs[0].label}>
+            <Tabs.List justify="flex-end">
+                {tabs.map((tab, index) => (
+                    <Tabs.Tab key={index} value={tab.label}>
+                        <Text size="lg" fw={600}>
+                            {tab.label}
+                        </Text>
+                    </Tabs.Tab>
+                ))}
+            </Tabs.List>
+
+            {tabs.map((tab, index) => (
+                <Tabs.Panel key={index} value={tab.label}>
+                    {tab.content}
+                </Tabs.Panel>
+            ))}
+        </Tabs>
+    );
+};
+
+export default TabHeader;
