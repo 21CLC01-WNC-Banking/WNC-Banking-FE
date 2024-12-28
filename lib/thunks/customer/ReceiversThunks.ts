ecdfa540a8e22e497ec4b7bc8a84383bcd6c9cbd
qhuongng <nq.huong.2304@gmail.com>
2024-12-28 22:32:56 +0700

    Add API thunks for receivers table

--- lib/thunks/customer/ReceiversThunks.ts
+++ lib/thunks/customer/ReceiversThunks.ts
@@ -3,22 +3,55 @@ import { setReceivers } from "../../slices/customer/ReceiversSlice";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-export const getReceiversThunk = createAppAsyncThunk("receivers/get", async (_, { dispatch }) => {
-    const response = await fetch(`${apiUrl}/customer/saved-receiver/`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-    });
+export const getReceiversThunk = createAppAsyncThunk(
+    "receivers/get-all",
+    async (_, { dispatch }) => {
+        const response = await fetch(`${apiUrl}/customer/saved-receiver/`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+        });
 
-    if (!response.ok) {
+        if (!response.ok) {
+            const responseData = await response.json();
+            throw new Error(responseData.errors[0].message || "Đã xảy ra lỗi kết nối với máy chủ.");
+        }
+
         const responseData = await response.json();
-        throw new Error(responseData.errors[0].message || "Đã xảy ra lỗi kết nối với máy chủ.");
+
+        dispatch(setReceivers(responseData.data || []));
     }
+);
 
-    const responseData = await response.json();
+export const getReceiverNameThunk = createAppAsyncThunk(
+    "receivers/get-name",
+    async (data: { accountNumber: string }) => {
+        const deformatted = data.accountNumber.split(" ").join("");
 
-    dispatch(setReceivers(responseData.data));
-});
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/account/customer-name?accountNumber=${deformatted}`,
+            {
+                method: "GET",
+                headers: { "Content-Type": "application/json" },
+                credentials: "include",
+            }
+        );
+
+        if (!response.ok) {
+            const responseData = await response.json();
+            let message = responseData.errors[0].message;
+
+            if (message.includes("no rows")) {
+                message = "Không tìm thấy người nhận. Vui lòng kiểm tra lại số tài khoản.";
+            }
+
+            throw new Error(message || "Đã xảy ra lỗi kết nối với máy chủ.");
+        }
+
+        const responseData = await response.json();
+        return responseData.data.name;
+    }
+);
 
 export const addInternalReceiverThunk = createAppAsyncThunk(
     "transfer/add-internal-receiver",
